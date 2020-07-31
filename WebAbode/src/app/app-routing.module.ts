8eb6c0acba4c44b145e36a65a3a9cfa80e734f93
Oishin Smith <oishin.smith25@mail.dcu.ie>
2020-07-31 19:50:36 +0100

    added changes

--- WebAbode/src/app/app-routing.module.ts
+++ WebAbode/src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { IntroComponent } from './intro/intro.component';
+import { ContentComponent } from './content/content.component';
+import { ThankyouComponent } from './thankyou/thankyou.component';
+import { PreviousWorkComponent } from './previous-work/previous-work.component';
+import { DonationsComponent } from './donations/donations.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SocialComponent } from './social/social.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/Home", pathMatch: "full" },
+  { path: "Home", component: HeaderComponent},
+  { path: "About", component: IntroComponent},
+  { path: "Gallery", component: GalleryComponent},
+  { path: "Services", component: ContentComponent},
+  { path: "The team", component: ThankyouComponent},
+  { path: "Portfolio", component: PreviousWorkComponent},
+  { path: "Pricing", component: DonationsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
